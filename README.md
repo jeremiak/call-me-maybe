663caee11fea69f7b5907008957053af63b3de95
Jeremia Kimelman <jbkimelman@gmail.com>
2015-06-29 22:17:45 -0400

    demos and docs

---
+++ README.md
@@ -0,0 +1,8 @@
+# Call Me Maybe
+
+A quick prototype for a research tool concept.
+
+## Demo
+
+[Here is an example of a page with the embedded script](https://jeremiak.github.io/call-me-maybe/demo.html) and the [resulting data store aka Google Doc](https://docs.google.com/spreadsheets/d/1uov31VpYtkCzLF0eFz6FVdC7y-JjcrX0sWTWE0ZunD8/edit#gid=1073603305).
+
--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ var _ = {
   template: require('lodash/string/template')
 };
 
-var postUrl = 'http://crossorigin-me-gwqynjms41pa.runscope.net/https://docs.google.com/forms/d/1PKyhJ1D06Gh-1ZE5bY4alYpLm6-GQxglJ-4xaSHjXdE/formResponse';
+var requestUrl = 'http://crossorigin.me/http://docs.google.com/forms/d/1PKyhJ1D06Gh-1ZE5bY4alYpLm6-GQxglJ-4xaSHjXdE/formResponse';
 var html = _.template(fs.readFileSync(__dirname + '/modal.html').toString());
 
 var id = window.cmm.id || 'noId';
@@ -25,9 +25,7 @@ modal.innerHTML = html({ id: id, roles: roles });
 modal.querySelector('button').addEventListener('click', onSubmit);
 modal.querySelector('#ccm-close').addEventListener('click', onClose);
 
-show();
-
-function show() {
+if (Math.floor(Math.random()*101) < showPercentage) {
   document.querySelector('body').appendChild(modal);
 }
 
@@ -50,10 +48,15 @@ function onSubmit(e) {
   data[m['email']] = form.querySelector('#ccm-email').value;
   data['fbzx'] = '3474659780927381613';
 
+  var formEncoded = Object.keys(data).map(function(key) {
+    return key + '=' + data[key];
+  }).join('&');
+
+  console.log('formEncoded', formEncoded);
+
   var requestData = {
-    method: 'POST',
-    uri: postUrl,
-    body: JSON.stringify(data)
+    method: 'GET',
+    uri: requestUrl + '?' + formEncoded
   };
 
   console.log('requestData', requestData);
@@ -61,7 +64,6 @@ function onSubmit(e) {
   xhr(requestData, function(e, r, b) {
     console.log('e', e);
     console.log('r', r);
-    console.log('b', b);
   });
 }
 
